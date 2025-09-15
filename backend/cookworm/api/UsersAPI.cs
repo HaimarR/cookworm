b6fd81a318d569f2a705c0093a9dc7e8211ee6b9
Haimar <haimarrubio24@gmail.com>
2025-09-14 22:00:13 -0400

    users api endpoint

---
+++ backend/cookworm/api/UsersAPI.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Cookworm.Models;
+using Cookworm.Services;
+
+namespace Cookworm.API
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersAPI : ControllerBase
+    {
+        private readonly UserService _userService;
+
+        public UsersAPI(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("signup")]
+        public IActionResult SignUp([FromBody] SignUpRequest request)
+        {
+            var user = _userService.SignUp(request);
+            if (user == null)
+                return BadRequest("Email already in use.");
+
+            // For now, just return success + user ID
+            return Ok(new { user.Id, user.Username, user.Email });
+        }
+    }
+}
